a62ef24d02fd29a44dc30ef0cb13136e7fb1e41c
Gabriel Uchoa <gabriel.uchoa.vasconcelos@gmail.com>
2020-07-25 14:59:37 -0300

    questao04 feita

--- Tarefa01/questao04/src/App.js
+++ Tarefa01/questao04/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import { Pai } from './Components/Pai';
 function App() {
   return (
     <div className="App">
-      
+      <Pai name='Gabriel' course='ES' city='Fortaleza'></Pai>
     </div>
   );
 }
--- Tarefa01/questao04/src/Components/Filho.jsx
+++ Tarefa01/questao04/src/Components/Filho.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react';
 
 
-class Filho extends Component {
-    render(){
-        return(
-        <h1>Nome : </h1>
+export default class Filho extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Nome: {this.props.name}</h1>
+                <h1>Curso: {this.props.course}</h1>
+                <h1>Cidade: {this.props.city}</h1>
+            </div>
         )
     }
 }
+
+export {Filho};

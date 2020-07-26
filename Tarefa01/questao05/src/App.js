b843e5821438253c6def8ad22c5d5d3cd35c3581
Gabriel Uchoa <gabriel.uchoa.vasconcelos@gmail.com>
2020-07-25 22:54:35 -0300

    mudando o card da questao05

--- Tarefa01/questao05/src/App.js
+++ Tarefa01/questao05/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 import Pai from './Components/Pai'
-import Card from 'react-bootstrap/Card'
 function App() {
   return (
     <div className="App">
-      <Card>
-        <Card.Body><Pai name='Carlos' course='EC' city='Crato'></Pai></Card.Body>
-      </Card>
+       
+        <Pai name='Carlos' course='EC' city='Crato'></Pai>
+      
     </div>
   );
 }
--- Tarefa01/questao05/src/Components/Filho.jsx
+++ Tarefa01/questao05/src/Components/Filho.jsx
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
-
+import Card from 'react-bootstrap/Card'
 
 export default class Filho extends Component {
     render() {
         return (
             <div>
-                <h1>Nome: {this.props.name}</h1>
-                <h1>Curso: {this.props.course}</h1>
-                <h1>Cidade: {this.props.city}</h1>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>Nome: {this.props.name}</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">Curso: {this.props.course}</Card.Subtitle>
+                        <Card.Text>
+                            Cidade: {this.props.city}
+                        </Card.Text>
+                        <Card.Link href="#">Card Link</Card.Link>
+                        <Card.Link href="#">Another Link</Card.Link>
+                    </Card.Body>
+                </Card>
             </div>
         )
     }
 }
 
-export {Filho};
+export { Filho };

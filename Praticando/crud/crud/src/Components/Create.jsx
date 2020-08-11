937e13c0de710993c3cdde7835165dbc1739a7fb
Gabriel Uchoa de Vasconcelos <gabriel.uchoa.vasconcelos@gmail.com>
2020-08-11 03:53:19 -0300

    adicionando Json-server

--- Praticando/crud/crud/src/Components/Create.jsx
+++ Praticando/crud/crud/src/Components/Create.jsx
@@ -1,43 +1,57 @@
 import React, { Component } from 'react';
+import Axios from 'axios'
+export default class Create extends Component {
 
-export default class Create extends Component{
-
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state = {nome: ''}
-        this.state = {curso: ''}
-        this.state = {IRA: ''}
+        this.state = { nome: '' }
+        this.state = { curso: '' }
+        this.state = { IRA: '' }
 
         this.setNome = this.setNome.bind(this)
         this.setCurso = this.setCurso.bind(this)
         this.setIra = this.setIra.bind(this)
         this.onSubmit = this.onSubmit.bind(this)
-    
-      }
-    
-      setNome(e){
-        this.setState({nome:e.target.value})
-      }
-      setCurso(e){
-        this.setState({curso:e.target.value})
-      }
-      setIra(e){
-        this.setState({IRA:e.target.value})
-      }
 
-      onSubmit(e){
-          e.preventDefault()
-          console.log('Nome: ' + this.state.nome)
-          console.log('Curso: ' + this.state.curso)
-          console.log('IRA: ' + this.state.IRA)
-          this.setState({nome: ''})
-          this.setState({curso: ''})
-          this.setState({IRA: ''})
-      }
+    }
 
-    render(){
-        return(
-            <div style={{marginTop:10}}>
+    setNome(e) {
+        this.setState({ nome: e.target.value })
+    }
+    setCurso(e) {
+        this.setState({ curso: e.target.value })
+    }
+    setIra(e) {
+        this.setState({ IRA: e.target.value })
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+        /*console.log('Nome: ' + this.state.nome)
+        console.log('Curso: ' + this.state.curso)
+        console.log('IRA: ' + this.state.IRA)
+        this.setState({nome: ''})
+        this.setState({curso: ''})
+        this.setState({IRA: ''})*/
+        const novoEstudante = {
+            nome: this.state.nome,
+            curso: this.state.curso,
+            IRA: this.state.IRA
+        }
+        Axios.post('http://localhost:3001/estudantes', novoEstudante)
+            .then(
+                (res) => { console.log('Estudante cadastrado com sucesso') }
+            )
+            .catch(
+                (error) => {
+                    console.log('Falha ao cadastrar novo estudante')
+                }
+            )
+    }
+
+    render() {
+        return (
+            <div style={{ marginTop: 10 }}>
                 <h3>Criar Estudante</h3>
                 <form onSubmit={this.onSubmit}>
                     <div className="form-group">

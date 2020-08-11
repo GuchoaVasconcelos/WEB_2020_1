c49ad67318f85df7884590863105c3b1d214e3f6
Gabriel Uchoa de Vasconcelos <gabriel.uchoa.vasconcelos@gmail.com>
2020-08-11 03:35:27 -0300

    ajeitando um bug e implementando json-server

--- atividade_3/src/Components/Create.jsx
+++ atividade_3/src/Components/Create.jsx
@@ -1,43 +1,60 @@
 import React, { Component } from 'react';
+import Axios from 'axios'
+export default class Create extends Component {
 
-export default class Create extends Component{
-
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state = {nome: ''}
-        this.state = {curso: ''}
-        this.state = {capacidade: ''}
+        this.state = { nome: '' }
+        this.state = { curso: '' }
+        this.state = { capacidade: '' }
 
         this.setNome = this.setNome.bind(this)
         this.setCurso = this.setCurso.bind(this)
         this.setCapacidade = this.setCapacidade.bind(this)
         this.onSubmit = this.onSubmit.bind(this)
-    
-      }
-    
-      setNome(e){
-        this.setState({nome:e.target.value})
-      }
-      setCurso(e){
-        this.setState({curso:e.target.value})
-      }
-      setCapacidade(e){
-        this.setState({capacidade:e.target.value})
-      }
 
-      onSubmit(e){
-          e.preventDefault()
-          console.log('Nome: ' + this.state.nome)
-          console.log('Curso: ' + this.state.curso)
-          console.log('Capacidade: ' + this.state.capacidade)
-          this.setState({nome: ''})
-          this.setState({curso: ''})
-          this.setState({IRA: ''})
-      }
+    }
 
-    render(){
-        return(
-            <div style={{marginTop:10}}>
+    setNome(e) {
+        this.setState({ nome: e.target.value })
+    }
+    setCurso(e) {
+        this.setState({ curso: e.target.value })
+    }
+    setCapacidade(e) {
+        this.setState({ capacidade: e.target.value })
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+        /*console.log('Nome: ' + this.state.nome)
+        console.log('Curso: ' + this.state.curso)
+        console.log('Capacidade: ' + this.state.capacidade)
+        this.setState({nome: ''})
+        this.setState({curso: ''})
+        this.setState({capacidade: ''})*/
+        const novaDisciplina = {
+            nome: this.state.nome,
+            curso: this.state.curso,
+            capacidade: this.state.capacidade
+        }
+        Axios.post('http://localhost:3001/disciplinas', novaDisciplina)
+        .then(
+            (res) =>{
+                console.log('Disciplina inserida com sucesso.')
+            }
+        )
+        .catch(
+            (error) => {
+                console.log(error)
+            }
+        )
+
+    }
+
+    render() {
+        return (
+            <div style={{ marginTop: 10 }}>
                 <h3>Criar Estudante</h3>
                 <form onSubmit={this.onSubmit}>
                     <div className="form-group">
